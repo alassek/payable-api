bf9e6e0e56edeb71e04d16445b2d2675093321a3
Adam Lassek <adam@doubleprime.net>
2016-11-09 17:03:13 -0600

    Fix type errors in Payable::Work

--- lib/payable/types.rb
+++ lib/payable/types.rb
@@ -6,6 +6,6 @@ module Payable
 
     QuantityMeasure = Strict::String.enum('time', 'money', 'unit', 'distance')
     InviteStatus    = Strict::String.enum('invited', 'not_invited', 'accepted')
-    PaymentStatus   = Strict::String.enum('draft_ready', 'payment_pending', 'payment_failed', 'payment_refunded', 'paid')
+    PaymentStatus   = Strict::String.enum('draft_ready', 'payment_pending', 'payment_failed', 'payment_refunded', 'paid').optional
   end
 end
--- lib/payable/work.rb
+++ lib/payable/work.rb
@@ -2,7 +2,7 @@ module Payable
   class Work < Resource
     attribute :worker_id, Types::Strict::Int
     attribute :work_type_id, Types::Strict::Int
-    attribute :quantity, Types::Coercible::Decimal
+    attribute :quantity, Types::Form::Decimal
     attribute :quantity_measure, Types::QuantityMeasure
     attribute :start, Types::Form::DateTime
     attribute :end, Types::Form::DateTime
